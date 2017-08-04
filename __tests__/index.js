d81d938439f43178e47f487c82a24ccb480afbf2
KennethMa <billyma128@gmail.com>
2017-08-04 18:34:01 +0800

    add basic structure

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,12 +1,12 @@
 import 'react-native';
 import React from 'react';
-import Index from '../index.android.js';
-
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import ReadhubClient from '../src/index';
+
 
 it('renders correctly', () => {
   const tree = renderer.create(
-    <Index />
+    <ReadhubClient />,
   );
 });
