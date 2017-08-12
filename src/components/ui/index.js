bb752880bdd2de5dc47f6b670b2be2d6d3462602
KennethMa <billyma128@gmail.com>
2017-08-12 16:01:31 +0800

    [T] move TabIcon to components directory;rename test file to *.test.js

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,3 +1,4 @@
 export Button from './Button';
 export Spacer from './Spacer';
 export Text from './Text';
+export TabIcon from './TabIcon';
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { View, Text } from 'react-native';
 import { Actions, Scene } from 'react-native-router-flux';
-import { Icon } from 'react-native-elements';
 import { HomeContainer, SettingsContainer } from '../containers';
 import AppConfig from '../constants/config';
-import { AppSizes, AppStyles, AppColors } from '../theme';
+import { AppSizes, AppStyles } from '../theme';
 import Placeholder from '../components/general/Placeholder';
+import { TabIcon } from '../components/ui';
 
 /* eslint-disable react/prefer-stateless-function */
 
@@ -17,26 +15,6 @@ const navbarPropsTabs = {
     paddingBottom: AppSizes.tabbarHeight,
   },
 };
-
-class TabIcon extends React.Component {
-  render() {
-    return (
-      <View>
-        <Icon name={this.props.iconName} type={this.props.iconType} color={this.props.selected ? AppColors.brand.primary : '#767676'} />
-        <Text style={{ color: this.props.selected ? AppColors.brand.primary : '#767676' }}>{this.props.title}</Text>
-      </View>
-    );
-  }
-}
-
-TabIcon.propTypes = {
-  title: PropTypes.string.isRequired,
-  selected: PropTypes.bool,
-  iconName: PropTypes.string.isRequired,
-  iconType: PropTypes.string.isRequired,
-};
-
-TabIcon.defaultProps = { title: 'Home', selected: false, iconName: 'home', iconType: 'ionicon' };
 
 export default Actions.create(
   <Scene key="root" {...AppConfig.navbarProps}>
